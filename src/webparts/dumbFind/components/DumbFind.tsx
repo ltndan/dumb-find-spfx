df29094913d264759622ea13f0b4b289ee5f0da2
Dan Deaconu <dan.deaconu@outlook.com>
2019-07-02 14:26:30 +0200

    finished logic

--- src/webparts/dumbFind/components/DumbFind.tsx
+++ src/webparts/dumbFind/components/DumbFind.tsx
@@ -2,12 +2,13 @@ import * as React from "react";
 import styles from "./DumbFind.module.scss";
 import { IDumbFindProps } from "./IDumbFindProps";
 import SearchBar from "../components/searchBar/searchBar";
-import pnp, { SearchQuery, SearchResults, SearchQueryBuilder, SearchProperty } from "sp-pnp-js";
+import pnp from "sp-pnp-js";
 import { ResultItem } from "../models/resultItem";
 import ResultItemView from "../components/resultItem/resultItemView";
 
 interface IDumbFindState {
   resultItems: ResultItem[];
+  loading: boolean;
 }
 
 export default class DumbFind extends React.Component<IDumbFindProps, IDumbFindState> {
@@ -16,30 +17,32 @@ export default class DumbFind extends React.Component<IDumbFindProps, IDumbFindS
 
     this.state = {
       resultItems: [],
+      loading: false,
     };
   }
 
   public componentDidMount() {
-    this.searchItems("*");
+    this.searchItems("");
   }
 
   private async searchItems(searchQuery: string) {
+    this.setState({ loading: true });
     const results = await pnp.sp.search({
-      Querytext: `Path:"https://ltdan.sharepoint.com/sites/TripDestinations/Destinations library*" AND FileType:jpg AND Title:"*${searchQuery}*"`,
+      Querytext: `${searchQuery}* (Path:"https://ltdan.sharepoint.com/sites/TripDestinations/Destinations library*" AND FileType:jpg)`,
       SelectProperties: ["DefaultEncodingURL", "Title", "Price"],
     });
     const primaryResults = results.PrimarySearchResults;
     const resultItems = primaryResults.map(x => {
       return { imageUrl: x["DefaultEncodingURL"], title: x.Title, price: x["Price"] };
     });
-    this.setState({ resultItems });
+    this.setState({ resultItems, loading: false });
   }
 
   public render(): React.ReactElement<IDumbFindProps> {
     return (
       <React.Fragment>
         <SearchBar onSearch={value => this.searchItems(value)} />
-        <section className={styles.resultsContainer}>
+        <section className={`${styles.resultsContainer} ${this.state.loading ? styles.blurred : ""}`}>
           {this.state.resultItems.map(x => (
             <ResultItemView {...x} />
           ))}
